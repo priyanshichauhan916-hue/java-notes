257c4f781c3e53eee4c0f757f6261740d6c2ad7b
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-01-31 10:12:00 +0530

    first commit

--- untitled/src/com/eetal/Inputs.java
+++ untitled/src/com/eetal/Inputs.java
@@ -1,3 +1,20 @@
-void main() {
-  $END$
+package com.eetal;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args){
+        Scanner input =  new Scanner(System.in);
+            System.out.print("Please enter some input: ");
+            int rollno = input.nextInt();
+            System.out.println("ur roll number is " +  rollno);
+
+        String name = input.next();
+        System.out.println(name);
+
+        float marks = input.nextFloat();
+        System.out.println(marks);
+
+    }
 }
+
--- untitled/src/com/eetal/Primitives.java
+++ untitled/src/com/eetal/Primitives.java
@@ -1,3 +1,16 @@
-void main() {
-  $END$
+package com.eetal;
+public class Primitives {
+   public static void main(String[] args) {
+       int rollno = 51;
+        char letter = 'e';
+        float marks = 98.33f;
+        double largeDecimalNumbers = 123456789.987654;
+        long largeInteger=34567893456745678L;
+        boolean check = true;
+        //String is not primitive
+        String name = "eetal";
+
+
+
+    }
 }
