671fdc82dfa5dc9c842c69252f54dbf57fc171b4
ankitisactive-pcs <ankitisactive@gmail.com>
2026-06-25 22:47:55 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+package src;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println(args[0]) ;
+    }
+}
