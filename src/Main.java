dcb6fb4ea576eb85c282dcd9c5316366a119e8d8
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-06-26 23:07:23 +0530

    learned java arrays

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
-package src;
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.println(args[0]) ;
+        String name1= "priyanshi chauhan";
+
+        //syntax for array
+        //datatype[] variable_name = new datatype[size]
+        int[] age1= new int[5];
+        //or directly
+        int[] name2= {5,6,7,7,8};
+        String[] array = new String[4];
+        System.out.println(array[1]);
+
+
+
+
     }
 }
