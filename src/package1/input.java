358c96624d2fd5d32ac47d8e27b0eecfaf7ebb84
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-06-28 22:35:28 +0530

    arrays day 2

---
+++ src/package1/input.java
@@ -0,0 +1,32 @@
+package package1;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args, int i) {
+        Scanner in= new Scanner(System.in);
+        int[] arr = new int[6];
+        //input using for loops
+        for(int i=0; i <arr.length; i++){
+            arr[i] = in.nextInt();
+
+        //print values entred
+        for(int i=0; i<arr.length; ++i) {
+            System.out.print(arr[i] + " ") ;
+        }
+
+        for(int num:arr){
+            System.out.println(num + " ") ;
+        }
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
