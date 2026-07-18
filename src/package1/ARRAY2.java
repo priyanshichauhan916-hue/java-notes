1b318d758521d498203b1b11a79782ca194be943
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-07-18 23:19:07 +0530

    ARRAY3

--- src/package1/ARRAY2.java
+++ src/package1/ARRAY2.java
@@ -1,14 +1,20 @@
 package package1;
 
+import java.util.Arrays;
+
 public class ARRAY2 {
     public static void main(String[] args) {
        int[][] arr = new int[][]{{1,2,3},{4,5,6}};
+       for(int[] a:arr){
+           System.out.println(Arrays.toString(a));
+       }
 //        printing rows and columns in a 2D array
-for( int row=0; row< arr.length; row++){
-    for (int col=0; col<arr[row].length; col++){
-        System.out.print(arr[row][col]+ " ");
-    }
-}
+//for( int row=0; row< arr.length; row++){
+//    for (int col=0; col<arr[row].length; col++){
+//        System.out.print(arr[row][col]+ " ");
+//    }
+//}
+
 
 
     }
---
+++ src/package1/ARRAY3.java
@@ -0,0 +1,7 @@
+package package1;
+import java.util.ARRAY3;
+public class ARRAY3 {
+    static void main(String[] args) {
+
+    }
+}
