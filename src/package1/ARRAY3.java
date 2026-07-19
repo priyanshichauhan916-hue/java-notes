7baf5ed72f3746c1acf650860f80d0b4dc0809c5
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-07-19 23:56:20 +0530

    arraylist2

--- src/package1/ARRAY3.java
+++ src/package1/ARRAY3.java
@@ -1,15 +1,14 @@
 package package1;
-import java.util.ARRAY3;
 public class ARRAY3 {
     static void main(String[] args) {
-        int[][] arr={
-            {1,2,3},
-            {4,5},
-            {6}
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5},
+                {6}
         };
-        for(int row=0; row<arr.length; row++)
-            for(int col=0; col<arr[row];col++)
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++)
                 System.out.print(arr[row][col]);
+        }
     }
-        System.out.print();
 }
