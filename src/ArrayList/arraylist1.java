4e7ab7ef9141fa6c57ebaa5fe58ee2a2d5bfee78
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-07-20 21:21:37 +0530

    adding 5 digits to array

--- src/ArrayList/arraylist1.java
+++ src/ArrayList/arraylist1.java
@@ -1,4 +1,16 @@
 package ArrayList;
+//for taking input of 5 terms by for loop
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class arraylist1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(in.nextInt());
+
+
+        }
+    }
 }
