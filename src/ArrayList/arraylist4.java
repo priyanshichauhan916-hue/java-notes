33d3f2e32895654543887746643791ad3903e474
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-07-22 00:52:42 +0530

    arraylist4

---
+++ src/ArrayList/arraylist4.java
@@ -0,0 +1,13 @@
+package ArrayList;
+
+import java.util.Arrays;
+
+public class arraylist4 {
+    public static void main(String[] args) {
+        int[] arr= {1,3,5,7,8,9};
+        int p=max(arr);
+        System.out.println(max(arr));
+
+    }
+
+}
