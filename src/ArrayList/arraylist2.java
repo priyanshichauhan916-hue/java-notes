4fdbd61b9f6d89908bf522660c45eb71dcb62480
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-07-22 00:18:15 +0530

    multidimensional arrays

--- src/ArrayList/arraylist2.java
+++ src/ArrayList/arraylist2.java
@@ -1,4 +1,18 @@
 package ArrayList;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class arraylist2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for(int i =0; i<5; i++){
+            list.add(new ArrayList<>());
+        }
+        for(int j=0;j<5;j++){
+            list.get(j).add(in.nextInt());
+        }
+        System.out.println(list);
+
+
+    }
 }
