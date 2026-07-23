1d6be8abf6f43734de2565bbd53397b0cbaf97e5
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-07-23 19:43:42 +0530

    maximum find

--- src/ArrayList/arraylist4.java
+++ src/ArrayList/arraylist4.java
@@ -1,3 +1,5 @@
+//for finding max
+
 package ArrayList;
 
 import java.util.Arrays;
@@ -5,9 +7,16 @@ import java.util.Arrays;
 public class arraylist4 {
     public static void main(String[] args) {
         int[] arr= {1,3,5,7,8,9};
-        int p=max(arr);
         System.out.println(max(arr));
-
+    }
+    static int max(int[] arr){
+        int maxValue=arr[0];
+        for(int i=1; i < arr.length; i++){
+            if (arr[i] > maxValue){
+                maxValue = arr[i];
+            }
+        }
+        return maxValue;
     }
 
 }
