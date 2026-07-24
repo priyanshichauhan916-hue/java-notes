6771c8a4f39271180e97f08678244a1d95f586ab
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-07-24 22:58:14 +0530

    random

--- src/ArrayList/arraylist4.java
+++ src/ArrayList/arraylist4.java
@@ -6,10 +6,34 @@ import java.util.Arrays;
 
 public class arraylist4 {
     public static void main(String[] args) {
-        int[] arr= {1,3,5,7,8,9};
-        System.out.println(max(arr));
+        int[] arr= {8,3,5,6,8,9};
+       // System.out.println(max(arr));
+        System.out.println(maxRage(arr, 1, 3));
     }
+
+    //writing the function maxRange
+    static int maxRage(int[] arr, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+        if (arr == null) {
+            return -1;
+        }
+        int maxValue = arr[start];
+        for (int j = start; j <= end; j++) {
+            if (arr[j] > maxValue) {
+                maxValue = arr[j];
+            }
+        }
+        return maxValue;
+    }
+
     static int max(int[] arr){
+        if (arr.length ==0){
+            return -1;
+        }
+
+
         int maxValue=arr[0];
         for(int i=1; i < arr.length; i++){
             if (arr[i] > maxValue){
---
+++ src/ArrayList/arraylist5.java
@@ -0,0 +1,10 @@
+//reversing the arrayyyy
+
+package ArrayList;
+
+public class arraylist5 {
+    public class void main(String[] args){
+        we
+
+    }
+}
