e8677191e973b06fd3cf9abe99fd6323b60e2c00
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-07-29 22:57:01 +0530

    arraypractice0

--- src/Practicearray/pa1.java
+++ src/Practicearray/pa1.java
@@ -1,4 +1,26 @@
-package Practicearray;
+package pa1;
 
-public class pa1 {
+class Solution{
+    public int countodd(int[] arr, int n) {
+        int count = 0;
+        for(int i=0 ; i< n; i++){
+            if (arr[i] % 2 == 1){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main (String[] args){
+        Solution s= new Solution();
+        int[] arr= {1,3,5,56,34};
+        int n = arr.length;
+
+        int count = s.countodd(arr, n);
+        System.out.println(count);
+    }
 }
+
+
+
+
+
