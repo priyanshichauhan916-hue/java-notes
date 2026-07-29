39444f805344969862042d73b397140ee1994fbf
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-07-29 22:34:54 +0530

    arraypractice0

--- src/ArrayList/arraylist5.java
+++ src/ArrayList/arraylist5.java
@@ -1,9 +1,27 @@
-//reversing the arrayyyy
+//sum of array
 
-package ArrayList;
-
-public class arraylist5 {
-    public class void main(String[] args){
-dghjkfghjghjgit
+//defining the function
+class Sol {
+    int sum(int arr[], int n) {
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = ans + arr[i];
+        }
+        return ans;
     }
 }
+//code
+public class arraylist5 {
+    public static void main(String[] args){
+        int n = 5;
+        int arr[] = {1,2,4,6,7};
+
+        // applying the function
+        Sol addition = new Sol();
+        int result = addition.sum(arr, n);
+        System.out.println(result);
+
+
+    }
+}
+
