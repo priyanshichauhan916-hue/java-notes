698f5fc75edbf66a39cbfe66b2ce2ce197a553c1
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-08-12 01:23:50 +0530

    sortedornot

--- src/Practicearray/pa2.java
+++ src/Practicearray/pa2.java
@@ -1,12 +1,31 @@
 //check if the array is sorted
 
-package Practicearray;
-
-public class pa2 {
-    public arraySortedorNot(int[] arr, int n){
-        for(int i=0 ; i< arr.length - 1; i++){
-            if(arr[i]>arr[i+1]){}
+class pa2 {
+    public boolean arraySortedOrNot(int[] arr, int n) {
+        for (int i =0; i<arr.length -1; i++) {
+            for (int j=0;j<arr.length; j++){
+                if (arr[i] > arr[j]) {
+                    return false;
+                }
+            }
         }
+        return true;
     }
+
+    public static void main(String[] args) {
+        pa2 solution= new pa2();
+
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        int n = arr.length;
+
+        boolean sorted = solution.arraySortedOrNot(arr, n);
+
+        if(sorted){
+            System.out.println("Sorted array");
+        } else{
+            System.out.println("Not sorted array");
+        }
+
+    }
+
 }
-g
