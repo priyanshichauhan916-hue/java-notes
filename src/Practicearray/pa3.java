99fd1b130f20ddd004d8e716cb8da3e96e082bd5
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-08-12 23:35:20 +0530

    reverse

--- src/Practicearray/pa3.java
+++ src/Practicearray/pa3.java
@@ -1,4 +1,14 @@
 package Practicearray;
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class pa3 {
+    public void reverse(int arr[], int n) {
+        int[] ans = new int[n];
+        for(int i=n-1; i >=0;i--){
+            
+        }
+
+    }
 }
+
+
