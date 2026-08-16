462f811ddc6314e6bb6e6706ab52376c5a9bd2ba
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-08-16 23:05:42 +0530

    LS2_2

---
+++ src/Linearsearch/LS2_1.java
@@ -0,0 +1,20 @@
+package Linearsearch;
+
+public class LS2_1 {
+    public static void main(String[] args) {
+        String name = "Eetal is legit";
+        char target = 'e';
+        System.out.println(search(name,target));
+    }
+    static boolean search(String str, char target) {
+        if(str.length()==0){
+            return false;
+        }
+        for(int i=0; i<str.length(); i++){
+            if(target== str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
