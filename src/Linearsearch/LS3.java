d82f9db3e9350c59f6eae6d30743a463ed182742
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-08-18 23:30:55 +0530

    LS3

--- src/Linearsearch/LS3.java
+++ src/Linearsearch/LS3.java
@@ -1,5 +1,19 @@
+//to find minimum number in the array
+
 package Linearsearch;
 public class LS3 {
     public static void main(String[] args) {
+        int[] arr= {4,2,5,6,2,56,346,45};
+        System.out.println(min(arr));
+    }
+    static int min(int[] arr){
+        int ans = arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>ans){
+                ans=arr[i];
+            }
+        }
+        return ans;
+    }
 
 }
