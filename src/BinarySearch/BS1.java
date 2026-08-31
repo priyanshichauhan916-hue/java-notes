f89d6d604f72ea046590215c2b02af729da8a37e
priyanshichauhan916-hue <priyanshichauhan916@gmail.com>
2026-08-31 17:41:21 +0530

    corrected code

--- src/BinarySearch/BS1.java
+++ src/BinarySearch/BS1.java
@@ -2,6 +2,10 @@ package BinarySearch;
 
 public class BS1 {
     public static void main(String[] args){
+        int [] arr= {1,2,4,5,6,7,8,9,90};
+        int target=9;
+        int ans = binarysearch(arr,target);
+        System.out.println(ans);
 
     }
     // return the index
@@ -10,13 +14,16 @@ public class BS1 {
         int left =0;
         int right = arr.length-1;
         //left+right cant be used as it might exceed the integer values
-        int mid= left + (right-left)/2;
-        if(target==arr[mid]){
-            return mid;
-        }else if(target>arr[mid]){
-            left=mid+1;
-        }else {
-            right = mid - 1;
+        while(left<=right){
+            int mid= left + (right-left)/2;
+            if (target == arr[mid]) {
+                return mid;
+            } else if (target > arr[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
         }
         return -1;
     }
--- src/Linearsearch/LS5.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-class Solution {
-    public int secondmostfrequent(int[] nums, int target) {
-        int n = nums.length;
-
-    }
-}
